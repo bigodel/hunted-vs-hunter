5e0c97252adf13329e7042dac5f3f6ba4505062d
maxwillf <maxwilliam780@gmail.com>
2018-10-07 22:03:02 -0300

    changed a lot of things in fish, added a new ocean.getFishAt() and created the actor interface and the location class

---
+++ src/Actor.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package src;
+import java.util.List;
+/**
+ *
+ * @author cyborg
+ */
+public interface Actor {
+       /**
+        * Executa o comportamento regular do ator.
+        * @param newActors Uma lista para armazenar os atores criados recentemente.
+        */
+        void act(List<Actor> newActors);
+        /**
+         * 
+         * @return true 
+         */
+        boolean isAlive();
+}
